6add5cfdcf592929c76d0ee12ed898a6118a57b2
legeckas <60690586+legeckas@users.noreply.github.com>
2022-01-09 21:40:52 +0200

    #2 Added GenericResponse model.

---
+++ TeUserManagement/Models/Responses/GenericResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace TeUserManagement.Models.Responses
+{
+    public class GenericResponse<TModel>
+    {
+        public TModel? Content { get; set; }
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        public static GenericResponse<TModel?> GenerateResponse(int statusCode, TModel content = default!, string message = null)
+        {
+            return new GenericResponse<TModel?>
+            {
+                StatusCode = statusCode,
+                Content = content,
+                Message = message
+            };
+        }
+    }
+}
